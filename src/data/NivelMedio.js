244ac9a90f4c6a7bbf189a95fef2a023c25e3c4c
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-05-04 20:08:07 -0400

    Cambio de rutas de las imagenes

--- src/data/NivelMedio.js
+++ src/data/NivelMedio.js
@@ -3,29 +3,29 @@ export const kinesiologicalTipsNivelMedio = {
       cabezaYCuello: [
         {
           exercise: "Entrelaza tus dedos y coloca las manos detrás de la cabeza. Flexiona lentamente la cabeza y redondea un poco la espalda hasta notar sensación de estiramiento.",
-          image: '/src/assets/nivelMedio/cabezaCuello.jpg'
+          image: '/nivelMedio/cabezaCuello.jpg'
         },
         {
           exercise: "Pasando el brazo por encima de la cabeza, toque su oreja con la mano contraria. Lentamente, incline la cabeza y el cuello hacia el lado del brazo que está tirando. Repite 2 veces por lado.",
-          image: '/src/assets/nivelMedio/cabezaCuello2.jpg'
+          image: '/nivelMedio/cabezaCuello2.jpg'
         }
       ],
       hombros: [
         {
           exercise: "Estiramientos cruzados: Lleva un brazo estirado por delante del pecho y empujado suavemente con el otro brazo por encima del codo. (10 segundos x brazo)",
-          image: '/src/assets/nivelMedio/hombro.jpg'
+          image: '/nivelMedio/hombro.jpg'
         }
       ],
       columnaLumbar: [
         {
           exercise: "Torsión espinal sentada/o: Apoya una mano en la parte externa de la rodilla opuesta y gira el tronco suavemente. (10 segundos x lado)",
-          image: '/src/assets/nivelMedio/lumbar.jpg'
+          image: '/nivelMedio/lumbar.jpg'
         }
       ],
       espaldaYbrazos: [
         {
           exercise: "Estiramientos en la pared: De pie frente a una pared, apoya las manos y deja caer el pecho hacia abajo, alargando brazos y espalda. (15 segundos)",
-          image: '/src/assets/nivelMedio/espalda.jpg'
+          image: '/nivelMedio/espalda.jpg'
         }
       ]
     },
@@ -33,15 +33,15 @@ export const kinesiologicalTipsNivelMedio = {
     pausasActivas: [
       {
         exercise: "Balancea los brazos y mueve los hombros en círculos para liberar tensión.",
-        image: '/src/assets/nivelMedio/pausaActiva.jpg'
+        image: '/nivelMedio/pausaActiva.jpg'
       },
       {
         exercise: "Sube y baja talones lentamente, activando tus piernas. (10 repeticiones).",
-        image: '/src/assets/nivelMedio/pausaActiva2.jpg'
+        image: '/nivelMedio/pausaActiva2.jpg'
       },
       {
         exercise: "Siéntate en el suelo y junta las plantas de los pies, dejando que las rodillas caigan hacia los lados. Sujeta los tobillos con las manos y, sin forzar, deja que las piernas se abran suavemente. Mantén la posición durante 30 segundos, respirando de forma relajada.",
-        image: '/src/assets/nivelMedio/pausaActiva3.jpg'
+        image: '/nivelMedio/pausaActiva3.jpg'
       }
     ]
   };
